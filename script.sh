c7d38f49209dbc3054f31b93b6c616df48af0bcc
AdrienDesclaux <adrien.desclaux@outlook.fr>
2024-12-09 17:52:43 +0100

    modifed nginx image html entry page

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
 docker pull nginx
 
 docker run -d -p 80:80 --name nginx nginx
+
+docker cp index.html nginx:/usr/share/nginx/html/index.html
+
+docker stop nginx
+
+docker commit nginx nginx-modified
+
+docker run -d -p 80:80 --name nginx-modified nginx-modified
