4c17707bc42a35a46942e73df0306c253426d774
AdrienDesclaux <adrien.desclaux@outlook.fr>
2024-12-09 16:56:44 +0100

    nginx

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
-docker pull mongodb/atlas
-docker pull node
+docker pull nginx
 
-docker run -d -p 27017:27017 --name mongodb mongo
-docker run -d -p 3000:3000 --name node node
+docker run -d -p 80:80 --name nginx nginx
